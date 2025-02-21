984f7d0720fb9df81e6caf0c93eab02fb345c66e
Johen In <injunhyun11@gmail.com>
2025-02-21 12:27:31 +0900

    play횟수를 위한 coin_tomato 로고를 LobbyScreen에 삽입

--- app/src/main/java/com/android/exampke/diecipomodori/LobbyScreen.kt
+++ app/src/main/java/com/android/exampke/diecipomodori/LobbyScreen.kt
@@ -7,11 +7,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,6 +21,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ColorMatrix
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -67,6 +71,33 @@ fun LobbyScreen(navController: NavController) {
                     vibrate()
                 }
         )
+        Row(modifier = Modifier.align(Alignment.Center).padding(top = 30.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.coin_tomato),
+                contentDescription = "coin",
+                colorFilter = ColorFilter.colorMatrix(
+                    ColorMatrix().apply { setToSaturation(0.1f) }
+                ),
+                modifier = Modifier
+                    .clickable {
+                    }
+            )
+            Image(
+                painter = painterResource(id = R.drawable.coin_tomato),
+                contentDescription = "coin",
+                modifier = Modifier
+                    .clickable {
+                    }
+            )
+            Image(
+                painter = painterResource(id = R.drawable.coin_tomato),
+                contentDescription = "coin",
+                modifier = Modifier
+                    .clickable {
+                    }
+            )
+        }
+
 
         // 상단 로고: 화면 너비의 70%
         Image(
